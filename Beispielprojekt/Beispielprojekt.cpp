7efff0bb089400f95c2daac3414b48b4057690f8
Erik Hanst <erik.hanst@gmail.com>
2023-10-11 20:11:01 +0200

    Test

--- Beispielprojekt/Beispielprojekt.cpp
+++ Beispielprojekt/Beispielprojekt.cpp
@@ -5,12 +5,23 @@ class GameWindow : public Gosu::Window
 {
 public:
 	float player_x = 100, player_y = 300;
+	float g = 5;
+	float dy = -10;
 
 	GameWindow()
 		: Window(800, 600)
 	{
 		set_caption("Gosu Tutorial mit Git");
 	}
+
+	struct Player
+	{
+		Gosu::Image bild = Gosu::Image::Image("Bitmap.bmp");
+		float x = 50, y = 50;
+
+	};
+
+	Player p1;
 
 	// Wird bis zu 60x pro Sekunde aufgerufen.
 	// Wenn die Grafikkarte oder der Prozessor nicht mehr hinterherkommen,
@@ -26,20 +37,38 @@ public:
 		);
 
 		// Player
-		graphics().draw_rect(
+		/*graphics().draw_rect(
 			player_x, player_y,
 			20, 30,
 			Gosu::Color::BLACK,
 			5
-		);
+		);*/
 
+		/*Gosu::Image::Image("Bitmap.bmp").draw(
+			player_x, player_y,
+			5
+		);*/
+
+		p1.bild.draw(
+			p1.x, p1.y,
+			5
+		);
 	}
 
 	// Wird 60x pro Sekunde aufgerufen
 	void update() override
 	{
-		player_x += 1;
-		player_y += 0.5;
+		if (dy < 0)
+		{
+			dy += 0.25;
+		}
+		
+		p1.x += 0.5;
+		if (p1.y <= 200)
+		{
+			p1.y += g + dy;
+		}
+		
 	}
 };
 
